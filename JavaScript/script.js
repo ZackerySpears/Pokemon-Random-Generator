c183a77447394ebc700be62c4451c9cc8e48afc6
zackerymarkusrayspears <zackery.spears97@gmail.com>
2019-09-23 21:24:53 -0500

    Added img and begin working on Javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,6 @@
+// var randomPokemon = document.getElementById("pokemonGen");
+
+function randomPokemon() {
+    var randomNumber = Math.floor(Math.random()) * 10;
+    console.log(randomNumber);
+}
